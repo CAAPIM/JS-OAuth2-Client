bf247ecee2e3721b014ff6e84b8013d2c3e10283
Pradeep Kumar <kumpr19@ca.com>
2018-07-10 21:02:53 +0530

    fix: Added create stateID function required for LoginWithIDToken

--- src/utils.js
+++ src/utils.js
@@ -725,4 +725,11 @@ function getOAuthParams(configURL,profileId) {
     xhttp.open('GET',configURL, true);
     xhttp.send();
   });
+} 
+/************* Create a stateID string **************/
+function stateID() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4()+ s4() + s4();
 }
